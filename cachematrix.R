9898764d7716a5131a8fa5a167eedf7f905c6416
colt111 <cfloyd@gmail.com>
2014-06-08 12:50:54 -0400

    replaced stem functions with real functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions cache a matrix and its inverse to avoid potentially
+## expensive recalculation of inverse.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+# Takes a matrix, returns list of functions to get and set matrix and inverse
+# Note: to maintain integrity of cache, ONLY update matrix with set function
+makeCacheMatrix <- function (m = matrix()) {
+        inv <- NULL
+        get <- function () m
+        set <- function (new_m) {
+                if (!identical(new_m, m)) {
+                m <<- new_m
+                inv <<- NULL
+                }
+        }
+        getinv <- function () inv
+        setinv <- function (new_inv) inv <<- new_inv
+        list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#Returns a matrix that is the inverse of 'm', from cache if available, else
+# computed from underlying matrix, (updating cache before returning)
+cacheSolve <- function(m, ...) {
+        inv <- m$getinv()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- m$get()
+        inv <- solve(data, ...)
+        m$setinv(inv)
+        inv
 }
+
